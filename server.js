19f5eb530d2ee669768abbdabce565e7134d80c2
PlesendYT <milowisso@gmail.com>
2026-08-01 01:33:24 +0200

    t

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Statische Dateien ausliefern
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
